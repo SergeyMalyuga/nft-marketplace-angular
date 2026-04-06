85a1d5c4f889335a088aafb457a6b1e8db640bca
Sergey Malyuga <sergey@malyuga.ru>
2026-04-06 14:20:19 +0300

    feat(routes): настроить маршрутизацию

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {AppRoute} from './core/constants/const';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+  path: AppRoute.MAIN,
+  title: 'MainComponent',
+  loadComponent: () => import('./pages/main/main.component').then(m => m.MainComponent),
+}];
