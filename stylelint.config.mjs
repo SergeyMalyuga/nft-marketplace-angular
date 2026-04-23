5303be89be62cd7df66bd336116327914a73008b
Sergey Malyuga <sergey@malyuga.ru>
2026-04-23 15:12:16 +0300

    feat(stylelint): подключить обнаружение дублирующих правил

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -2,6 +2,12 @@
 export default {
   extends: ["stylelint-config-standard-scss"],
   rules: {
+    "no-duplicate-selectors": [
+      true,
+      {
+        message: "Обнаружены дублирующиеся селекторы",
+      },
+    ],
     "selector-class-pattern": [
       "^[a-z]([a-z0-9-]+)?(__[a-z0-9-]+)?(_--[a-z0-9-]+)?$",
       {
