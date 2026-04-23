1374154c6bbd1ac47c30c2434028e479b53aee97
Sergey Malyuga <sergey@malyuga.ru>
2026-04-23 15:19:24 +0300

    feat(hero): добавить текст и изображение

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,8 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {HeroStatsComponent} from '../hero-stats/hero-stats.component';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [
+    HeroStatsComponent
+  ],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
