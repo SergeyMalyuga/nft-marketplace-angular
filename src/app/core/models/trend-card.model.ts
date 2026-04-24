3f1c0de1591667a443bf15ac34113899f7b2be9d
Sergey Malyuga <sergey@malyuga.ru>
2026-04-24 13:55:12 +0300

    feat(trend-card): добавить модель карточкм популярного предложения

---
+++ src/app/core/models/trend-card.model.ts
@@ -0,0 +1,10 @@
+import {Image} from './image.model';
+import {Owner} from './owner.model';
+
+export interface TrendCard {
+  id: string;
+  previewImage: Image;
+  images: Image[];
+  title: string;
+  owner: Owner;
+}
