57903132bd3f46f8c8e56067fb721d9de2a9eeb5
Sergey Malyuga <sergey@malyuga.ru>
2026-04-27 13:27:02 +0300

    feat(home-page): добавить секцию лучших создателей

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -2,13 +2,15 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import {HeaderComponents} from '../../components/header/header.components';
 import {HeroComponent} from '../../components/hero/hero.component';
 import {TrendCollectionComponent} from '../../components/trend-collection/trend-collection.component';
+import {TopCreatorsComponent} from '../../components/top-creators/top-creators.component';
 
 @Component({
   selector: 'app-main',
   imports: [
     HeaderComponents,
     HeroComponent,
-    TrendCollectionComponent
+    TrendCollectionComponent,
+    TopCreatorsComponent
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
