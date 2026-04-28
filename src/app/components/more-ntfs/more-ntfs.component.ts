bbf6332e4f2d9de50c496e8cf87684a033812b81
Sergey Malyuga <sergey@malyuga.ru>
2026-04-28 16:10:57 +0300

    feat(more-ntfs): создать секцию коллекции NFT

---
+++ src/app/components/more-ntfs/more-ntfs.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-more-ntfs',
+  imports: [],
+  templateUrl: './more-ntfs.component.html',
+  styleUrl: './more-ntfs.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MoreNtfsComponent {}
