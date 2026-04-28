4fe597a866ae3f099292ecf403c1d67c4ce79e7d
Sergey Malyuga <sergey@malyuga.ru>
2026-04-28 14:57:49 +0300

    feat(categories): создать секцию с лучшими категориями

---
+++ src/app/components/categories/categories.component.ts
@@ -0,0 +1,16 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {CATEGORY_CARDS} from '../../core/constants/const';
+import {CategoryCardComponent} from '../category-card/category-card.component';
+
+@Component({
+  selector: 'app-categories',
+  imports: [
+    CategoryCardComponent
+  ],
+  templateUrl: './categories.component.html',
+  styleUrl: './categories.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CategoriesComponent {
+  protected readonly CATEGORY_CARDS = CATEGORY_CARDS;
+}
