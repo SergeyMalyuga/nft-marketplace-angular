2930c944497babfdc673a9e267c4448df611aa7c
Sergey Malyuga <sergey@malyuga.ru>
2026-04-29 15:23:47 +0300

    feat(more-ntfs): создать секцию дополнительной коллекции NFT

--- src/app/components/more-ntfs/more-ntfs.component.ts
+++ src/app/components/more-ntfs/more-ntfs.component.ts
@@ -1,10 +1,27 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {NFT_CARDS} from '../../core/constants/const';
+import {NftCardComponent} from '../../shared/components/nft-card/nft-card.component';
+import {SwiperComponents} from '../../shared/components/swiper/swiper.components';
+import {DEFAULT_COLLECTION_SWIPER} from '../../shared/config/swiper.config';
 
 @Component({
   selector: 'app-more-ntfs',
-  imports: [],
+  imports: [
+    NftCardComponent,
+    SwiperComponents
+  ],
   templateUrl: './more-ntfs.component.html',
   styleUrl: './more-ntfs.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MoreNtfsComponent {}
+export class MoreNtfsComponent {
+  protected readonly NFT_CARDS = NFT_CARDS;
+
+  public isSwiper = signal<boolean>(false);
+
+  public toggleList() {
+    this.isSwiper.set(!this.isSwiper());
+  }
+
+  protected readonly DEFAULT_COLLECTION_SWIPER = DEFAULT_COLLECTION_SWIPER;
+}
