db3ae4a957393018345c27e569050f5690dda8c6
Sergey Malyuga <sergey@malyuga.ru>
2026-04-29 15:21:13 +0300

    feat(swiper): добавить Input для базового CSS-класса

--- src/app/shared/components/swiper/swiper.components.ts
+++ src/app/shared/components/swiper/swiper.components.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, ChangeDetectionStrategy, Component, Input, OnDestroy} from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, Component, input, Input, OnDestroy} from '@angular/core';
 import Swiper from 'swiper';
 import {Pagination} from 'swiper/modules';
 import {SwiperOptions} from 'swiper/types';
@@ -12,10 +12,12 @@ import {SwiperOptions} from 'swiper/types';
 })
 export class SwiperComponents implements AfterViewInit, OnDestroy {
   @Input({required: true}) options!: SwiperOptions;
+  @Input({required: true}) swiperClass!: string;
+
   private swiper!: Swiper;
 
   ngAfterViewInit(): void {
-    this.swiper = new Swiper('.swiper', {
+    this.swiper = new Swiper(`.${this.swiperClass}`, {
       modules: [Pagination],
       slidesPerView: 'auto',
       ...this.options,
