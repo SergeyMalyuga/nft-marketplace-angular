140a04aca6f1e96d7641b34a2f88935657b5810d
Sergey Malyuga <sergey@malyuga.ru>
2026-04-30 14:50:59 +0300

    feat(about-card): создать компонент

---
+++ src/app/components/about-card/about-card.component.ts
@@ -0,0 +1,16 @@
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {AboutCard} from '../../core/models/about-card.model';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-about-card',
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './about-card.component.html',
+  styleUrl: './about-card.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AboutCardComponent {
+  @Input({required: true}) card!: AboutCard;
+}
