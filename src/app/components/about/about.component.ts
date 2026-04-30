8aa940bce578d303201af65c1850d3eca59637e6
Sergey Malyuga <sergey@malyuga.ru>
2026-04-30 14:51:44 +0300

    feat(about): создать секцию

---
+++ src/app/components/about/about.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ABOUT_CARDS} from '../../core/constants/const';
+import {AboutCardComponent} from '../about-card/about-card.component';
+
+@Component({
+  selector: 'app-about',
+  imports: [
+    AboutCardComponent
+  ],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AboutComponent {
+  protected readonly ABOUT_CARDS = ABOUT_CARDS;
+}
