3431bbe02a17d92171a683d22fe91e85a1dad859
Sergey Malyuga <sergey@malyuga.ru>
2026-05-05 13:09:00 +0300

    feat(subscribe): создать компонент

---
+++ src/app/components/subscribe/subscribe.ts
@@ -0,0 +1,10 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+
+@Component({
+  selector: 'app-subscribe',
+  imports: [],
+  templateUrl: './subscribe.html',
+  styleUrl: './subscribe.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Subscribe {}
