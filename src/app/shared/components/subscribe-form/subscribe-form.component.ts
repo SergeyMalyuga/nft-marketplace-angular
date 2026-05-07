f07fbec32c2bd94885a4e37c1bf3e17a11c25cab
Sergey Malyuga <sergey@malyuga.ru>
2026-05-07 14:19:57 +0300

    feat(subscribe-form): создать компонент подписки на новости

---
+++ src/app/shared/components/subscribe-form/subscribe-form.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subscribe-form',
+  imports: [],
+  templateUrl: './subscribe-form.component.html',
+  styleUrl: './subscribe-form.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SubscribeFormComponent {}
