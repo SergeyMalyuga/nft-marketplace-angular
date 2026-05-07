22af778b4e1775c5f6b36e9fe473726ae56ba55b
Sergey Malyuga <sergey@malyuga.ru>
2026-05-07 14:21:44 +0300

    feat(footer): создать компонент

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+import {SubscribeFormComponent} from '../../shared/components/subscribe-form/subscribe-form.component';
+
+@Component({
+  selector: 'app-footer',
+  imports: [
+    NgOptimizedImage,
+    SubscribeFormComponent
+  ],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FooterComponent {}
