a43c49a5bc86e826be6693734e07756b57bd0bd1
Sergey Malyuga <sergey@malyuga.ru>
2026-05-20 16:03:54 +0300

    feat(body): добавить сервис

---
+++ src/app/core/services/body.service.ts
@@ -0,0 +1,12 @@
+import {DOCUMENT, inject, Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BodyService {
+  private document = inject(DOCUMENT);
+
+  public setOverflow(isNoOverflow: boolean) {
+    this.document.body.classList.toggle('no-overflow', isNoOverflow);
+  }
+}
