71fa2b38f51e4a08476ba64880c2a60428452d3f
Sergey Malyuga <sergey@malyuga.ru>
2026-05-20 16:11:08 +0300

    feat(header): реализовать разблокировку прокрутки страницы
    - При переходе из мобильной версии в настольную

--- src/app/components/header/header.components.ts
+++ src/app/components/header/header.components.ts
@@ -1,4 +1,6 @@
-import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, effect, inject, signal} from '@angular/core';
+import {BodyService} from '../../core/services/body.service';
+import {ScreenService} from '../../core/services/screen.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,23 @@ import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponents {
+  private bodyService = inject(BodyService);
+  private screenService = inject(ScreenService);
+
   public isNavMenuOpen = signal<boolean>(false);
+
+  constructor() {
+    effect(() => {
+      const isTablet = this.screenService.isTablet();
+      if (!isTablet) {
+        this.isNavMenuOpen.set(false);
+        this.bodyService.setOverflow(this.isNavMenuOpen());
+      }
+    });
+  }
 
   public toggleNavMenu() {
     this.isNavMenuOpen.set(!this.isNavMenuOpen());
+    this.bodyService.setOverflow(this.isNavMenuOpen());
   }
 }
