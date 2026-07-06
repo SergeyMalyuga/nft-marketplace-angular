3d98556d5ae1439b2e5a64a4b33552d8cf9cc2dd
Sergey Malyuga <sergey@malyuga.ru>
2026-07-06 15:39:56 +0300

    feat(nft-card): настроить навигацию на страницу artist
    - При клике на карточку

--- src/app/shared/components/nft-card/nft-card.component.ts
+++ src/app/shared/components/nft-card/nft-card.component.ts
@@ -1,11 +1,14 @@
 import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import {NftCard} from '../../../core/models/nft-card.model';
 import {NgOptimizedImage} from '@angular/common';
+import {RouterLink} from '@angular/router';
+import {AppRoute} from '../../../core/constants/const';
 
 @Component({
   selector: 'app-nft-card',
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    RouterLink
   ],
   templateUrl: './nft-card.component.html',
   styleUrl: './nft-card.component.scss',
@@ -13,4 +16,5 @@ import {NgOptimizedImage} from '@angular/common';
 })
 export class NftCardComponent {
   @Input({required: true}) card!: NftCard;
+  protected readonly AppRoute = AppRoute;
 }
