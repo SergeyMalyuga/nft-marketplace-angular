53b8c3e547c1e4b981451c16e6e327cf8a9cfdf4
Sergey Malyuga <sergey@malyuga.ru>
2026-07-08 10:18:38 +0300

    feat(header): добавить список карточек

--- src/app/pages/marketplace/marketplace.component.ts
+++ src/app/pages/marketplace/marketplace.component.ts
@@ -1,15 +1,20 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import {HeaderComponents} from '../../shared/components/header/header.components';
 import {FooterComponent} from '../../shared/components/footer/footer.component';
+import {NFT_CARDS} from '../../core/constants/const';
+import {NftCardComponent} from '../../shared/components/nft-card/nft-card.component';
 
 @Component({
   selector: 'app-marketplace',
   imports: [
     HeaderComponents,
-    FooterComponent
+    FooterComponent,
+    NftCardComponent
   ],
   templateUrl: './marketplace.component.html',
   styleUrl: './marketplace.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MarketplaceComponent {}
+export class MarketplaceComponent {
+  protected readonly NFT_CARDS = NFT_CARDS;
+}
