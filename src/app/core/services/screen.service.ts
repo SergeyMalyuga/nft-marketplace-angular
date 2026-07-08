a4f7651d76c94be89821832edc8cb5b267ec07fe
Sergey Malyuga <sergey@malyuga.ru>
2026-07-08 11:45:59 +0300

    feat(screen): добавить определение мобильных устройств

--- src/app/core/services/screen.service.ts
+++ src/app/core/services/screen.service.ts
@@ -5,24 +5,34 @@ import {isPlatformBrowser} from '@angular/common';
   providedIn: 'root'
 })
 export class ScreenService implements OnDestroy {
-  private mediaQuery?: MediaQueryList;
+  private tabletQuery?: MediaQueryList;
+  private mobileQuery?: MediaQueryList;
+
   public isTablet = signal(false);
+  public isMobile = signal(false);
 
   constructor(@Inject(PLATFORM_ID) private platform: object) {
     if (isPlatformBrowser(platform)) {
-      this.mediaQuery = window.matchMedia("(max-width: 929px)");
-      this.isTablet.set(this.mediaQuery.matches);
-      this.mediaQuery.addEventListener('change', this.onChange)
+      this.tabletQuery = window.matchMedia("(max-width: 929px)");
+      this.isTablet.set(this.tabletQuery.matches);
+      this.tabletQuery.addEventListener('change', this.onTabletChange)
+
+      this.mobileQuery = window.matchMedia("(max-width: 768px)");
+      this.isMobile.set(this.mobileQuery.matches);
+      this.mobileQuery.addEventListener('change', this.onMobileChange);
     }
   }
 
-  private onChange = (evt: MediaQueryListEvent) => {
+  private onTabletChange = (evt: MediaQueryListEvent) => {
     this.isTablet.set(evt.matches);
   }
 
+  private onMobileChange = (evt: MediaQueryListEvent) => {
+    this.isMobile.set(evt.matches);
+  }
+
   ngOnDestroy(): void {
-    if (this.mediaQuery) {
-      this.mediaQuery.removeEventListener('change', this.onChange);
-    }
+    this.tabletQuery?.removeEventListener('change', this.onTabletChange);
+    this.mobileQuery?.removeEventListener('change', this.onTabletChange);
   }
 }
