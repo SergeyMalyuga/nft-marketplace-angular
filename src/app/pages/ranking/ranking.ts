0915f6105dcdaa7b9b0c64b79bd586657acfcfa4
Sergey Malyuga <sergey@malyuga.ru>
2026-07-08 11:55:32 +0300

    feat(ranking): добавить фильтры для карточек

--- src/app/pages/ranking/ranking.ts
+++ src/app/pages/ranking/ranking.ts
@@ -1,15 +1,49 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, inject, signal} from '@angular/core';
 import {HeaderComponents} from '../../shared/components/header/header.components';
 import {FooterComponent} from '../../shared/components/footer/footer.component';
+import {ScreenService} from '../../core/services/screen.service';
+import {RankingFilter} from '../../core/models/ranking-filter.model';
+import {NgClass} from '@angular/common';
+import {Filter} from '../../core/constants/const';
 
 @Component({
   selector: 'app-ranking',
   imports: [
     HeaderComponents,
-    FooterComponent
+    FooterComponent,
+    NgClass
   ],
   templateUrl: './ranking.html',
   styleUrl: './ranking.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Ranking {}
+export class Ranking {
+  public screenService = inject(ScreenService);
+  public currentFilter = signal<Filter>(Filter.TODAY);
+  public filtersText = computed<RankingFilter[]>(() => [
+    {
+      id: 'Today',
+      label: this.screenService.isMobile() ? '1d' : 'Today',
+      type: Filter.TODAY
+    },
+    {
+      id: 'This Week',
+      label: this.screenService.isMobile() ? '7d' : 'This Week',
+      type: Filter.THIS_WEEK
+    },
+    {
+      id: 'This Month',
+      label: this.screenService.isMobile() ? '30d' : 'This Month',
+      type: Filter.THIS_MONTH
+    },
+    {
+      id: 'All Time',
+      label: 'All Time',
+      type: Filter.ALL_TIME
+    }
+  ]);
+
+  public changeFilter(filter: Filter) {
+    this.currentFilter.set(filter);
+  }
+}
